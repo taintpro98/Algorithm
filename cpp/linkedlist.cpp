6127c4efef0954bcca31001b75d52c514ccb74a5
BatmanBruceWayne <taideptrai02@gmail.com>
2020-10-02 11:10:30 +0700

    update linkedlist

--- cpp/linkedlist.cpp
+++ cpp/linkedlist.cpp
@@ -18,24 +18,6 @@ void print_list(Node* n)
     } 
 } 
 
-/* Given a reference (pointer to pointer)  
-to the head of a list and an int,  
-inserts a new node on the front of the list. */
-void push(Node** head_ref, int new_data)  
-{  
-    /* 1. allocate node */
-    Node* new_node = new Node();  
-  
-    /* 2. put in the data */
-    new_node->data = new_data;  
-  
-    /* 3. Make next of new node as head */
-    new_node->next = (*head_ref);  
-  
-    /* 4. move the head to point to the new node */
-    (*head_ref) = new_node;  
-}  
-
 void insert_after(Node* prev_node, int new_data)   
 { 
     
@@ -95,39 +77,11 @@ void append(Node** head_ref, int new_data)
     return;  
 }  
 
-void test_append(Node* head_ref, int new_data)  
+/* Given a reference (pointer to pointer)  
+to the head of a list and an int,  
+inserts a new node on the front of the list. */
+void push(Node** head_ref, int new_data)  
 {  
-    /* 1. allocate node */
-    Node* new_node = new Node(); 
-  
-    Node *last = head_ref; /* used in step 5*/
-  
-    /* 2. put in the data */
-    new_node->data = new_data;  
-  
-    /* 3. This new node is going to be  
-    the last node, so make next of  
-    it as NULL*/
-    new_node->next = NULL;  
-  
-    /* 4. If the Linked List is empty, 
-    then make the new node as head */
-    if (head_ref == NULL)  
-    {  
-        head_ref = new_node;  
-        return;  
-    }  
-  
-    /* 5. Else traverse till the last node */
-    while (last->next != NULL)  
-        last = last->next;  
-  
-    /* 6. Change the next of last node */
-    last->next = new_node;  
-    return;  
-}  
-
-void test_push(Node* head, int new_data){
     /* 1. allocate node */
     Node* new_node = new Node();  
   
@@ -135,11 +89,11 @@ void test_push(Node* head, int new_data){
     new_node->data = new_data;  
   
     /* 3. Make next of new node as head */
-    new_node->next = head;  
+    new_node->next = (*head_ref);  
   
     /* 4. move the head to point to the new node */
-    head = new_node;
-}
+    (*head_ref) = new_node;  
+}  
   
 // Driver code 
 int main() 
@@ -162,14 +116,8 @@ int main()
     third->data = 3; // assign data to third node 
     third->next = NULL; 
 
-    // push(&head, 4);
-    // push(&head, 4);
-
-    // test_push(head, 5);
-    // test_push(head, 5);
-
+    push(&head, 4);
     append(&head, 4);
-    test_append(head, 5);
     print_list(head);
 
     return 0; 
